e08ea69dd61bab3e8a87701006c3f06b43464254
Jasmer Singh Sanjotra <jasmer.sanjotra@gmail.com>
2024-06-13 16:33:53 -0500

    Fixed errors

--- datasets/dataset_lits.py
+++ datasets/dataset_lits.py
@@ -47,35 +47,6 @@ class RandomGenerator(object):
         sample = {'image': image, 'label': label.long()}
         return sample
 
-
-class Synapse_dataset(Dataset):
-    def __init__(self, base_dir, list_dir, split, transform=None):
-        self.transform = transform  # using transform in torch!
-        self.split = split
-        self.sample_list = open(os.path.join(list_dir, self.split+'.txt')).readlines()
-        self.data_dir = base_dir
-
-    def __len__(self):
-        return len(self.sample_list)
-
-    def __getitem__(self, idx):
-        if self.split == "train":
-            slice_name = self.sample_list[idx].strip('\n')
-            data_path = os.path.join(self.data_dir, slice_name+'.npz')
-            data = np.load(data_path)
-            image, label = data['image'], data['label']
-        else:
-            vol_name = self.sample_list[idx].strip('\n')
-            filepath = self.data_dir + "/{}.npy.h5".format(vol_name)
-            data = h5py.File(filepath)
-            image, label = data['image'][:], data['label'][:]
-
-        sample = {'image': image, 'label': label}
-        if self.transform:
-            sample = self.transform(sample)
-        sample['case_name'] = self.sample_list[idx].strip('\n')
-        return sample
-    
 class RandomRotationTransform:
     def __init__(self, degrees=(0, 180), size=(224,224)):
         self.degrees = degrees
@@ -88,13 +59,6 @@ class RandomRotationTransform:
         image = F.rotate(image, angle)
         mask = F.rotate(mask, angle)
         return image, mask
-
-def augfun(image, mask):
-    angle = random.uniform(0, 180)
-    rotate_transform = transforms.RandomRotation(degrees=(0, 180))
-    image = transforms.functional.rotate(image, angle)
-    mask = transforms.functional.rotate(mask, angle)
-    return image, mask
 
 class LITSDataset(Dataset):
     def __init__(self,Xlist,Ylist,transform):
